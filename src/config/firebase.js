6117f6525146231674897733ff36c46427aac67a
Tze Wei <tzeweiwee@gmail.com>
2019-10-06 01:21:17 +0800

    added firebase config newsletter textbox component

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSGia0iFv_pVx10kzEbLuer5CkoweN8B8",
+  authDomain: "discovernative-web.firebaseapp.com",
+  databaseURL: "https://discovernative-web.firebaseio.com",
+  projectId: "discovernative-web",
+  storageBucket: "discovernative-web.appspot.com",
+  messagingSenderId: "555829801523",
+  appId: "1:555829801523:web:0746511d3d2140393c6946",
+  measurementId: "G-JMBBJBVBB0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
