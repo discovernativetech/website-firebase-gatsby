30e6fc0822fbb33aa65106dee07286fdb0a16dbf
Tze Wei <tzeweiwee@gmail.com>
2020-02-20 20:00:41 +0800

    remove old pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,51 +1,51 @@
-const path = require("path")
-const experiencePage = path.resolve(`./src/templates/Experience/experience.js`)
+// const path = require("path")
+// const experiencePage = path.resolve(`./src/templates/Experience/experience.js`)
 
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    {
-      allTravellersJson {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-  result.data.allTravellersJson.edges.forEach(({ node }) => {
-    console.log(JSON.stringify(node, null, 4));
-    createPage({
-      path: node.slug,
-      component: experiencePage,
-      context: {
-        slug: node.slug,
-        node,
-      },
-    })
-  })
-}
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+//   const { createPage } = actions
+//   const result = await graphql(`
+//     {
+//       allTravellersJson {
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+//   result.data.allTravellersJson.edges.forEach(({ node }) => {
+//     console.log(JSON.stringify(node, null, 4));
+//     createPage({
+//       path: node.slug,
+//       component: experiencePage,
+//       context: {
+//         slug: node.slug,
+//         node,
+//       },
+//     })
+//   })
+// }
 
-exports.onCreateWebpackConfig = ({
-  stage,
-  actions,
-  getConfig
-}) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, 'umd ' + request);
-        }
-        callback();
-      })
-    });
-  }
-};
+// exports.onCreateWebpackConfig = ({
+//   stage,
+//   actions,
+//   getConfig
+// }) => {
+//   if (stage === 'build-html') {
+//     actions.setWebpackConfig({
+//       externals: getConfig().externals.concat(function(context, request, callback) {
+//         const regex = /^@?firebase(\/(.+))?/;
+//         // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+//         if (regex.test(request)) {
+//           return callback(null, 'umd ' + request);
+//         }
+//         callback();
+//       })
+//     });
+//   }
+// };
