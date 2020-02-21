8ca859e4132ef8d58bec8143287dc20cb9e6ea34
Tze Wei <tzeweiwee@gmail.com>
2020-02-21 21:32:03 +0800

    remove unused imports

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { Link, useStaticQuery, graphql  } from "gatsby";
-import { Link  } from "gatsby";
+// import { Link  } from "gatsby";
 
 import { MDBContainer } from 'mdbreact';
 // import Img from "gatsby-image";
